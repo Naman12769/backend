08520c486f6896d89eaf0092563e4014e38f9e98
Naman <namanjainjain341@gmail.com>
2024-08-21 17:40:00 +0530

    last commit

--- controllers/user.js
+++ controllers/user.js
@@ -3,109 +3,85 @@ const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
 const { sendCookie } = require('../utils/features');
 exports.getAllusers=async(req,res)=>{
- try {
-  const {id}=req.params;
-  const task=await Task.find(id);
-  if(!task){
-    return next(new ErrorHandler("task not found!",404))
-   }
-  task.iscompleted=!task.iscompleted;
-  await task.save();
-  res.status(201).json({
+  const user=await User.find({});
+  // console.log(req.query);
+  // const keyword=req.query.keyword;
+  // console.log(keyword);
+  res.json({
     success:true,
-    message:"task updated"
+    data:user
   })
- } catch (error) {
-  next(error);
- }
 }
 
 exports.register=async(req,res)=>{
-  try {
-    const {id}=req.params;
-  const task=await Task.find(id);
-  if(!task){
-    return next(new ErrorHandler("task not found!",404))
-   }
-  task.iscompleted=!task.iscompleted;
-  await task.save();
-  res.status(201).json({
-    success:true,
-    message:"task updated"
-  })
-  } catch (error) {
-    next(error);
+  const {name,email,password}=req.body;
+  let user=await User.findOne({email});
+  // console.log(req.query);
+  // const keyword=req.query.keyword;
+  // console.log(keyword);
+  if(user){
+    return res.status(404).json({
+      success:false,
+      message:"user already exists"
+
+    })
   }
+  const hashedPassword=await bcrypt.hash(password,10);
+
+  await User.create({name,email,password:hashedPassword});
+  sendCookie(user,res,"registered successfully!",201);
  
 }
 exports.login=async(req,res)=>{
-  try {
-    const {id}=req.params;
-  const task=await Task.find(id);
-  if(!task){
-    return next(new ErrorHandler("task not found!",404))
-   }
-  task.iscompleted=!task.iscompleted;
-  await task.save();
-  res.status(201).json({
-    success:true,
-    message:"task updated"
-  })
-  } catch (error) {
-    next(error);
+  const {email,password}=req.body;
+  const user=await user.findOne({email}).select("+password");
+  if(!user){
+    return res.status(404).json({
+      success:false,
+      message:"invalid email and password"
+    })
   }
+  const ismatch=await bcrypt.compare(password,user.password);
+  if(!ismatch){
+    return res.status(404).json({
+      success:false,
+      message:"invalid email and password"
+    })
+  }
+  sendCookie(user,res,"welcome back!",200);
 }
 exports.getMyProfile=async(req,res)=>{
   
 
   // console.log(token);
 
-  try {
-    res.status(200).cookie("token","",{
-      expires:new Date(Date.now())
-    }).json({
-      success:true,
-      user:req.user
-    })
-  } catch (error) {
-    next(error);
-  }
+  res.status(200).json({
+    success:true,
+    user:req.user
+  })
 }
 exports.updateUser=async(req,res)=>{
-  try {
-    res.status(200).cookie("token","",{
-      expires:new Date(Date.now())
-    }).json({
-      success:true,
-      user:req.user
-    })
-  } catch (error) {
-    next(error);
-  }
+  const {id}=req.params;
+  const user=await User.findById(id);
+  res.json({
+    success:true,
+    message:"user updated"
+  })
 }
 exports.deleteUser=async(req,res)=>{
- try {
-  res.status(200).cookie("token","",{
-    expires:new Date(Date.now())
-  }).json({
+  const {id}=req.params;
+  const user=await User.findById(id);
+  user.deleteOne();
+  res.json({
     success:true,
-    user:req.user
+    message:"user deleted"
   })
- } catch (error) {
-  next(error);
- }
 }
 exports.logout=(req,res,next)=>{
-try {
-  res.status(200).cookie("token","",{
-    expires:new Date(Date.now()),
-    sameSite:process.env.NODE_ENV==="Devlopment"?"lax":"none",
-    secure:process.env.NODE_ENV==="Development"?false:true
-  }).json({
-    success:true,
-    user:req.user
-  })
-} catch (error) {
-  next(error);
-}
+res.status(200).cookie("token","",{
+  expires:new Date(Date.now())
+}).json({
+  success:true,
+  user:req.user
+})
 }
